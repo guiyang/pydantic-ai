081b3ee6b8cc03737b7b03ba4839a64dd7a98898
Yang Gui <guiyang923@gmail.com>
2025-02-28 17:02:56 +0800

    Update openai.py

--- pydantic_ai_slim/pydantic_ai/models/openai.py
+++ pydantic_ai_slim/pydantic_ai/models/openai.py
@@ -215,7 +215,7 @@ class OpenAIModel(Model):
         if not tools:
             tool_choice: Literal['none', 'required', 'auto'] | None = None
         elif not model_request_parameters.allow_text_result:
-            tool_choice = 'required'
+            tool_choice = 'auto'
         else:
             tool_choice = 'auto'
 
